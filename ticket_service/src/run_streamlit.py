fcb75a83b6e18728b79d485f0ed2736f7cf73acb
SelezGit <selezard@mail.ru>
2022-07-11 17:48:17 +0300

    fix: delete deprecated folder

--- ticket_service/src/run_streamlit.py
+++
@@ -1,5 +0,0 @@
-from view.main_view import ViewApp
-
-if __name__ == "__main__":
-    view = ViewApp()
-    view.run()
--- ticket_service/src/view/__init__.py
+++
@@ -1,5 +0,0 @@
-from .create import CreateView  # noqa
-from .change import ChangeView  # noqa
-from .delete import DeleteView  # noqa
-from .manager import ManagerView  # noqa
-from .faq import FAQView  # noqa
